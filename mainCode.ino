58056629c95a3bd1a12beeb663fd93bf7bb7879a
SAM33RG <sameergangar1@gmail.com>
2017-12-22 00:49:02 +0530

    added 2 relay code fixed a bug

--- mainCode.ino
+++ mainCode.ino
@@ -4,7 +4,7 @@
 #define R1 8
 #define ON 1
 #define OFF 0
-#define R2 9
+#define R2 10
 
 
 
@@ -28,6 +28,7 @@ void setup()
 {       
   Serial.begin(9600);  
   pinMode(R1, OUTPUT);
+  pinMode(R2, OUTPUT);
 
 }
 
@@ -38,11 +39,17 @@ void loop(){
     Serial.println(command);
     
     switch(command){
-    case 'F':  
+    case '1':  
       relay(ON,R1);
       break;
-    case 'B':  
+    case '2':  
       relay(OFF,R1);
+      break;
+    case '3':  
+      relay(ON,R2);
+      break;
+    case '4':  
+      relay(OFF,R2);
       break;
     }
   } 
